b508dcd1a9effb89140fefdb197b540412b11e4a
Joseph Blais <josephpierreblais@gmail.com>
2021-05-03 18:07:40 -0400

    Add html & state logic to Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import useDebounce from "../hooks/useDebounce";
+
 export default function Searchbar() {
+  const [value, setValue] = useState("");
+  const term = useDebounce(value, 500);
+
+  // const onSearch = useCallback(props.onSearch, [term]);
 
   return (
-    <p>Hello World - Search Bar</p>
+    <>
+      <form onSubmit={(event) => event.preventDefault()}>
+        {/* This label is hidden but is visible for screen readers */}
+        <label htmlFor="search-bar">
+          <span className="visually-hidden">Search Films</span>
+        </label>
+        <input
+          type="text"
+          id="search-bar"
+          placeholder="Search Films"
+          onChange={(event) => setValue(event.target.value)}
+          name="s"
+        />
+      </form>
+      <h2>{term}</h2>
+    </>
   );
 }
